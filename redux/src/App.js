ec962a91e290696bae3573c9772127aada261980
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-05-09 04:56:39 +0530

    DOCUMENTATION ALSO DONE

--- redux/src/App.js
+++ redux/src/App.js
@@ -15,3 +15,8 @@ function App() {
 }
 
 export default App;
+
+
+// NOTE-->
+
+// The Provider component in react-redux is used to make the Redux store available to your React application. It essentially "provides" the Redux store to any React component that needs to connect to it, allowing them to access Redux state and dispatch actions.
