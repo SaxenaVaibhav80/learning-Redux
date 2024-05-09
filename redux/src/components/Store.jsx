b5a4e46460389c63ae19cb9640a52f52eb333544
vaibhav saxena <121886758+SaxenaVaibhav80@users.noreply.github.com>
2024-05-09 05:49:11 +0530

    documentation also complete

--- redux/src/components/Store.jsx
+++ redux/src/components/Store.jsx
@@ -3,3 +3,24 @@ import BookReducer from './BookReducer'
 const store=createStore(BookReducer)
 
 export default store
+
+
+// for combining multiple reducer that hold state in a single store----->
+
+
+// import { createStore, combineReducers } from 'redux';
+// import userReducer from './reducers/userReducer';
+// import productReducer from './reducers/productReducer';
+
+//Combine multiple reducers into one------>
+
+// const rootReducer = combineReducers({
+//   user: userReducer,
+//   product: productReducer,
+// });
+
+// // Create the Redux store with the combined reducer---->
+
+// const store = createStore(rootReducer);
+
+// export default store;
